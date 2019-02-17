f37ea935101489403d3cfe1d72559b439658b596
Klanc <clank201@gmail.com>
2019-02-18 00:00:28 +0100

    Added a couple of FontAwesome Glyphs on the width testing chars array

--- src/rxvtfont.C
+++ src/rxvtfont.C
@@ -158,6 +158,7 @@ static uint16_t extent_test_chars[] = {
   '0', '1', '8', 'a', 'd', 'x', 'm', 'y', 'g', 'W', 'X', '\'', '_',
   0x00cd, 0x00d5, 0x0114, 0x0177, 0x0643,	// ÍÕĔŷﻙ
   0x304c, 0x672c,				// が本
+  0xf118, 0xf2c6,               // 
 };
 
 #define dTermDisplay Display *disp = term->dpy
